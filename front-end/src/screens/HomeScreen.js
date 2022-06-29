14b65fb5b87659e889fe067a0908f5bb479f9a77
parthu34 <parth.darji34@gmail.com>
2022-06-29 12:32:04 -0400

    ui-update

--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -39,7 +39,7 @@ const HomeScreen = {
           ${product.brand}
         </div>
         <div class="product-price">
-          $${product.price}
+        ₹${product.price}
         </div>
         </div>
       </li>
