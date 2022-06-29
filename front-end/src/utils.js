8fdd7aeaf7263bffb2f7c00756e9df62d29b5c2d
parthu34 <parth.darji34@gmail.com>
2022-06-29 08:56:35 -0400

    add-webpack

--- front-end/src/utils.js
+++ front-end/src/utils.js
@@ -1,9 +1,9 @@
-export const parseRequestUrl = () =>{
-    const url = document.location.hash.toLocaleLowerCase();
-    const request = url.split("/");
+export const parseRequestUrl = () => {
+    const url = document.location.hash.toLowerCase();
+    const request = url.split('/');
     return {
-        resource: request[1],
-        id: request[2],
-        action: request[3]
+      resource: request[1],
+      id: request[2],
+      action: request[3],
     };
-};
+  };
