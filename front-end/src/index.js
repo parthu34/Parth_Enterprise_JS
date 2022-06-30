6763e6567263a733734dafba32630802ceced232
parthu34 <parth.darji34@gmail.com>
2022-06-30 01:28:38 -0400

    update-user-login-api

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,3 +1,4 @@
+import Header from './components/Header';
 import CartScreen from './screens/CartScreen';
 import Error404Screen from './screens/Error404Screen';
 import HomeScreen from './screens/HomeScreen';
@@ -19,7 +20,9 @@ const router = async () => {
     (request.id ? '/:id' : '') +
     (request.verb ? `/${request.verb}` : '');
   const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
-
+  const header = document.getElementById('header-container');
+  header.innerHTML = await Header.render();
+  await Header.after_render();
   const main = document.getElementById('main-container');
   main.innerHTML = await screen.render();
   await screen.after_render();
