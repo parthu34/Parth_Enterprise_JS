1c678920f7330dfe871ec4ce0e3f93480313aabb
parthu34 <parth.darji34@gmail.com>
2023-01-28 17:14:45 -0500

    dashboard-update

--- front-end/src/screens/DashboardScreen.js
+++ front-end/src/screens/DashboardScreen.js
@@ -1,18 +1,40 @@
-import DashboardMenu from "../components/DashboardMenu";
+import DashboardMenu from '../components/DashboardMenu';
+import { getSummary } from '../api';
 
+let summary = {};
 const DashboardScreen = {
-    after_render: () => { },
-    render: () => `
-        <div class="dashboard">
-          ${DashboardMenu.render({ selected: 'dashboard' })}
-          <div class="dashboard-content">
-            <h1>Dashboard</h1>
-            <div>
-              Info and Charts will be added here
+  after_render: () => {},
+  render: async () => {
+    summary = await getSummary();
+    return `
+    <div class="dashboard">
+      ${DashboardMenu.render({ selected: 'dashboard' })}
+      <div class="dashboard-content">
+        <h1>Dashboard</h1>
+       
+        <ul class="summary-items">
+          <li>
+            <div class="summary-title color1">
+              <span><i class="fa fa-users"></i> Users</span>
             </div>
-          </div>
-        </div>
-        `,
+            <div class="summary-body">${summary.users[0].numUsers}</div>
+          </li>
+          <li>
+            <div class="summary-title color2">
+              <span><i class="fa fa-users"></i> Orders</span>
+            </div>
+            <div class="summary-body">${summary.orders[0].numOrders}</div>
+          </li>
+          <li>
+            <div class="summary-title color3">
+              <span><i class="fa fa-users"></i> Sales</span>
+            </div>
+            <div class="summary-body">₹${summary.orders[0].totalSales}</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    `;
+  },
 };
-
-export default DashboardScreen
+export default DashboardScreen;
